a33904b95778e81f1b7ef341d0b09e99556d56c4
Chandrabhan <chandrabhan0603@gmail.com>
2026-01-08 09:31:51 +0530

    developed the color generating functionality

--- color.js
+++ color.js
@@ -1,17 +1,43 @@
-let main = document.querySelector('.main')
-let randomColor = document.querySelector('.random-color')
-let colorInput = document.querySelector('#color-input')
-let applyColor = document.querySelector('.apply-color')
-let colorValue = document.querySelector('color-value')
+let main = document.querySelector("body");
+let randomColor = document.querySelector(".random-color");
+let colorInput = document.querySelector("#color-input");
+let applyColor = document.querySelector(".apply-color");
+let colorValue = document.querySelector(".color-value");
 
 // console.log(main,randomColor,colorInput,applyColor,colorValue)
+const colorArray = [
+  "white",
+  "black",
+  "green",
+  "Yellow",
+  "red",
+  "blue",
+  "orange",
+  "tomato",
+  "cyan",
+  "lightcoral",
+  "acqua"
+];
+const randomColorGenerator = () => {
+  let randomNumber = Math.floor(Math.random() * colorArray.length);
+  return (colorArray[randomNumber]);
+};
 
-function handleRandomColor(){
-    console.log("you have clicked the random color")
-}
-function handleApplyColor(){
-    console.log("you have clicked the apply color")
+
+const changeColor = (color) => {
+  main.style.backgroundColor = color;
+  colorValue.innerText = color;
+};
+
+function handleRandomColor() {
+let color = randomColorGenerator();
+changeColor(color)
 }
 
-randomColor.addEventListener('click', handleRandomColor)
-applyColor.addEventListener('click', handleApplyColor)
+function handleApplyColor() {
+  let color = colorInput.value;
+  changeColor(color);
+}
+
+randomColor.addEventListener("click", handleRandomColor);
+applyColor.addEventListener("click", handleApplyColor);
